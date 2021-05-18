d7f713d22ab51c434a3c6c101f4c6c9d6e7149de
PetraAtef <petra_atef@hotmail.com>
2021-05-18 21:24:47 +0200

    adding server wrokers

--- src/client/__test__/testHandleSubmit.spec.js
+++ src/client/__test__/testHandleSubmit.spec.js
@@ -2,7 +2,7 @@
 // https://knowledge.udacity.com/questions/174638
 import 'babel-polyfill'
 
-import event_handler from '../js/formHandler'
+import {event_handler} from '../js/formHandler'
 
 describe('Testing the submit event handlers functionality', () => {
   test('Testing the event_handler() function', () => {
